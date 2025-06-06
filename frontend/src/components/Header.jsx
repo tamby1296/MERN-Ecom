fd93ac7de3ba5948a7e5304f9f6cd35f9214c3f3
Tamby Tia RAZAFINDRALAMBO <tambytia.razafindralambo@elca.mu>
2025-06-06 11:54:56 +0400

    update to vite

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
+import logo from "../assets/images/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="logo" />
+            <span>ProShop</span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/cart">
+                <FaShoppingCart />
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                <FaUser />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import App from "./App.jsx";
+import HomeScreen from "./screens/Home/index.jsx";
+
+import "./assets/styles/bootstrap.custom.css";
+import "./assets//styles/index.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
